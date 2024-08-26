42ff09475043d386d60c44836572c50c6720ee30
enews <59407044+AieEiole@users.noreply.github.com>
2024-08-27 01:01:09 +0900

    2

--- Build/SampleMulti.framework.js
+++ Build/SampleMulti.framework.js
@@ -2002,13 +2002,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  4570816: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 4570877: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 4570941: function() {return Module.webglContextAttributes.powerPreference;},  
- 4570999: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 4571054: function($0) {performance.now = function() { return $0; };},  
- 4571102: function($0) {performance.now = function() { return $0; };},  
- 4571150: function() {performance.now = Module['emscripten_get_now_backup'];}
+  4570832: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 4570893: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 4570957: function() {return Module.webglContextAttributes.powerPreference;},  
+ 4571015: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 4571070: function($0) {performance.now = function() { return $0; };},  
+ 4571118: function($0) {performance.now = function() { return $0; };},  
+ 4571166: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
